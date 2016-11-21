69a1b826dd3a6533273db2056fb30ad41b701476
bilalbinrais <bilalbinrais@gmail.com>
2016-11-21 21:40:50 +0900

    Update AmbilWarnaDialog.java

--- library/src/main/java/yuku/ambilwarna/AmbilWarnaDialog.java
+++ library/src/main/java/yuku/ambilwarna/AmbilWarnaDialog.java
@@ -180,7 +180,7 @@ public class AmbilWarnaDialog {
 				}
 			}
 		})
-		.setNegativeButton(""취, new DialogInterface.OnClickListener() {
+		.setNegativeButton("취", new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				if (AmbilWarnaDialog.this.listener != null) {
